310ade56661f49d0a3d3edf99d18b2a2832473ee
AvishkarSaste <avishkarsaste40@gmail.com>
2025-06-29 23:42:20 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,24 +1,28 @@
 const express = require("express");
 const cors = require("cors");
-const {MongoClient}= require("mongodb");
+const { MongoClient } = require("mongodb");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
 	const url = "mongodb+srv://jasimanaaa:NGQZ53t8dlBXHpU9@cluster0.elq70da.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 	const con = new MongoClient(url);
-	const db = con.db("msg_28june25");
-	const coll = db.collection("messages");
-	coll.find().toArray()
-	.then(response=>{
-		const r = parseInt(Math.random()*response.length);
-		res.status(200).send(response[r]);
+
+	con.connect()
+	.then(() => {
+		const db = con.db("msg_28june25");
+		const coll = db.collection("messages");
+		return coll.find().toArray();
 	})
-	.catch(error=>{
+	.then(response => {
+		const r = parseInt(Math.random() * response.length);
+		res.status(200).send(response[r]);
+		con.close(); 	})
+	.catch(error => {
 		res.status(500).send(error);
 	});
 });
 
-app.listen(9000,()=>{console.log("ready to serve @ 9000");});
+app.listen(9000, () => { console.log("ready to serve @ 9000"); });
